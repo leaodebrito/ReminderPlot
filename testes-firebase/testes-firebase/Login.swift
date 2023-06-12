2bf8ca3dc021f421e4f3fa191ce75cc2cb34cb40
Bruno Leão <leaodebrito@icloud.com>
2023-06-11 21:23:55 -0300

    desenvolvimento de tela de login

--- testes-firebase/testes-firebase/Login.swift
+++ testes-firebase/testes-firebase/Login.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct Login: View {
+    
+    
+    @State var email: String = ""
+    @State var senha: String = ""
+    
+    
     var body: some View {
         VStack{
-            Image(systemName: "lock")
-                .resizable()
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Text("Login Page")
+                Text("Olá,")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.title)
+                    .bold()
+                
+                Text("Faça seu login.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .bold()
+                    .padding(.bottom, 30)
+            
+            
+            
+            TextField("E-mail", text: $email)
+                .frame(maxWidth: .infinity)
+                .textFieldStyle(.roundedBorder)
+            
+            SecureField("Senha", text: $senha)
+                .frame(maxWidth: .infinity)
+                .textFieldStyle(.roundedBorder)
         }
+        
+        .padding(.horizontal)
     }
 }
 
